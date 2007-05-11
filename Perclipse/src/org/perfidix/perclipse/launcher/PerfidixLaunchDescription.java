fd891f4affc0655874dca3a5cc817435b35fd935
sebastiangraf <none@none>
2007-05-11 10:02:23 +0000

    --HG-- extra : convert_revision : svn%3Af0255af0-1427-0410-b2be-e7b72c6cd545/trunk%4028

---
+++ Perclipse/src/org/perfidix/perclipse/launcher/PerfidixLaunchDescription.java
@@ -0,0 +1,42 @@
+package org.perfidix.perclipse.launcher;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.launching.IJavaLaunchConfigurationConstants;
+
+public class PerfidixLaunchDescription {
+
+	private Map<String,Object> fAttributes= new HashMap<String,Object>();
+
+	private final IJavaElement fElement;
+
+	private final String fName;
+	
+	public PerfidixLaunchDescription(IJavaElement element, String name) {
+		fElement= element;
+		fName= name;
+		setAttribute(IJavaLaunchConfigurationConstants.ATTR_PROJECT_NAME, getProjectName());
+	}
+	
+	private String getProjectName() {
+		IJavaProject project = getProject();
+		return project == null ? null : project.getElementName();
+	}
+	
+	private IJavaProject getProject() {
+		return fElement == null ? null : fElement.getJavaProject();
+	}
+	
+	private PerfidixLaunchDescription setAttribute(String attr, Object value) {
+		fAttributes.put(attr, value);
+		return this;
+	}
+	
+	public PerfidixLaunchDescription setMainType(IType mainType) {
+		return setAttribute(IJavaLaunchConfigurationConstants.ATTR_MAIN_TYPE_NAME, mainType);
+	}
+}
