304afbd0bfc2f9c74a13466f57d7a879815c6259
dbutnaru <devnull@localhost>
2007-05-27 13:40:43 +0000

    added persistent configuration storage

--- Perclipse/src/org/perfidix/perclipse/launcher/Perclipse.java
+++ Perclipse/src/org/perfidix/perclipse/launcher/Perclipse.java
@@ -1,5 +1,7 @@
 package org.perfidix.perclipse.launcher;
 
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
@@ -9,53 +11,65 @@ import org.osgi.framework.BundleContext;
  */
 public class Perclipse extends AbstractUIPlugin {
 
-	// The plug-in ID
-	public static final String PLUGIN_ID = "Perclipse";
+  // The plug-in ID
+  public static final String PLUGIN_ID = "Perclipse";
 
-	// The shared instance
-	private static Perclipse plugin;
-	
-	/**
-	 * The constructor
-	 */
-	public Perclipse() {
-		plugin = this;
-	}
+  // The shared instance
+  private static Perclipse plugin;
+  
+  /**
+   * The constructor
+   */
+  public Perclipse() {
+    plugin = this;
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-	}
+  /*
+   * (non-Javadoc)
+   * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+   */
+  public void start(BundleContext context) throws Exception {
+    super.start(context);
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		super.stop(context);
-	}
+  /*
+   * (non-Javadoc)
+   * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+   */
+  public void stop(BundleContext context) throws Exception {
+    plugin = null;
+    super.stop(context);
+  }
 
-	/**
-	 * Returns the shared instance
-	 *
-	 * @return the shared instance
-	 */
-	public static Perclipse getDefault() {
-		return plugin;
-	}
+  /**
+   * Returns the shared instance
+   *
+   * @return the shared instance
+   */
+  public static Perclipse getDefault() {
+    return plugin;
+  }
 
-	/**
-	 * Returns an image descriptor for the image file at the given
-	 * plug-in relative path
-	 *
-	 * @param path the path
-	 * @return the image descriptor
-	 */
-	public static ImageDescriptor getImageDescriptor(String path) {
-		return imageDescriptorFromPlugin(PLUGIN_ID, path);
-	}
+  public static String getPluginId() {
+    return PLUGIN_ID;
+  }
+
+  public static void log(Throwable e) {
+    log(new Status(IStatus.ERROR, getPluginId(), IStatus.ERROR, "Error", e));
+  }
+
+  public static void log(IStatus status) {
+    getDefault().getLog().log(status);
+  }
+  
+  /**
+   * Returns an image descriptor for the image file at the given
+   * plug-in relative path
+   *
+   * @param path the path
+   * @return the image descriptor
+   */
+  public static ImageDescriptor getImageDescriptor(String path) {
+    return imageDescriptorFromPlugin(PLUGIN_ID, path);
+  }
 }
