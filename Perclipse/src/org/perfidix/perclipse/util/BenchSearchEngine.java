2b7d8e4aed7a4dcae0582a14ae410a0703421a2a
sebastiangraf <none@none>
2007-05-29 08:29:04 +0000

    Perfidix Main Type implemented but not tested yet
    --HG--
    extra : convert_revision : svn%3Af0255af0-1427-0410-b2be-e7b72c6cd545/trunk%4058

--- Perclipse/src/org/perfidix/perclipse/util/BenchSearchEngine.java
+++ Perclipse/src/org/perfidix/perclipse/util/BenchSearchEngine.java
@@ -5,13 +5,31 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jdt.core.IClassFile;
+import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jface.operation.IRunnableContext;
+import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.ui.IFileEditorInput;
 public class BenchSearchEngine {
 
+	public static IType[] findBenchs(IRunnableContext context, final Object[] elements) throws InvocationTargetException, InterruptedException {
+		final Set result= new HashSet();
+
+		if (elements.length > 0) {
+			IRunnableWithProgress runnable= new IRunnableWithProgress() {
+				public void run(IProgressMonitor pm) throws InterruptedException {
+					BenchFinder.findBenchsInContainer(elements, result, pm);
+				}
+			};
+			context.run(true, true, runnable);
+		}
+		return (IType[]) result.toArray(new IType[result.size()]);
+	}
+	
 	public static Object computeScope(Object element) throws JavaModelException {
 		if (element instanceof IFileEditorInput)
 			element = ((IFileEditorInput) element).getFile();
@@ -34,4 +52,16 @@ public class BenchSearchEngine {
 		return (IType[]) result.toArray(new IType[result.size()]);
 	}
 
+	public static boolean hasBenchType(IJavaProject javaProject) {
+		return benchType(javaProject) != null;
+	}
+	
+	private static IType benchType(IJavaProject javaProject) {
+		try {
+			return javaProject.findType("org.perfidix.Bench"); //$NON-NLS-1$
+		} catch (JavaModelException e) {
+			return null;
+		}
+	}
+	
 }
