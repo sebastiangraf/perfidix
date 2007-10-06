fdf25e34a19a593f0e0fcca6023ac8c751e25bbd
sebastiangraf <devnull@localhost>
2007-10-06 13:44:17 +0000

    raw data output implemented

---
+++ src/org/perfidix/visitor/RawData.java
@@ -0,0 +1,82 @@
+package org.perfidix.visitor;
+
+import java.io.File;
+import java.io.FileWriter;
+
+import org.perfidix.IResult;
+import org.perfidix.Result;
+
+/**
+ * Storing the raw data without any computation in single files. Per method, one
+ * single file is opened with the data. Only usable with Benchmarkresult
+ * 
+ * @author sebi
+ * 
+ */
+public class RawData extends ResultVisitor {
+
+    private final File folder;
+
+    public RawData(final String pathToFolder) {
+        folder = new File(pathToFolder);
+        if (folder.exists()) {
+            if (!folder.isDirectory()) {
+                folder.delete();
+            } else {
+                deleteRecursive(folder);
+            }
+        }
+        folder.mkdir();
+    }
+
+    @Override
+    public void visit(Result r) {
+        if (!(r instanceof IResult.BenchmarkResult)) {
+            throw new RuntimeException("only benchmark results are supported!");
+        }
+        IResult.BenchmarkResult benchRes = (IResult.BenchmarkResult) r;
+        for (IResult.ClassResult classRes : benchRes.getChildren()) {
+            for (IResult.MethodResult methodRes : classRes.getChildren()) {
+                getMethodResult(methodRes);
+            }
+        }
+    }
+
+    private void getMethodResult(final IResult.MethodResult methodRes) {
+        try {
+            final File currentFile = new File(this.folder.getAbsolutePath()
+                    + "//" + methodRes.getName());
+            if (currentFile.exists()) {
+                throw new IllegalStateException(currentFile.getAbsolutePath()
+                        + " is already existing!");
+            }
+            currentFile.createNewFile();
+            final FileWriter out = new FileWriter(currentFile);
+            IResult.SingleResult single = methodRes.getChildren().get(0);
+            final long data[] = single.getResultSet();
+            for (int i = 0; i < data.length; i++) {
+                if (i == data.length - 1) {
+                    out.write(data[i] + " ");
+                } else {
+                    out.write(data[i] + ",");
+                }
+            }
+            out.flush();
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void deleteRecursive(final File file) {
+        if (file.isDirectory()) {
+            final File[] childs = file.listFiles();
+            for (int i = 0; i < childs.length; i++) {
+                deleteRecursive(childs[i]);
+            }
+        } else {
+            file.delete();
+        }
+    }
+
+}
