8a97c96b5334bcbaa8b1cd6fb29a0741be256d8d
sebastiangraf <devnull@localhost>
2008-02-13 08:41:28 +0000

    debugged RawData->different Classes, similar methods are now supported

--- src/org/perfidix/visitor/RawData.java
+++ src/org/perfidix/visitor/RawData.java
@@ -1,4 +1,3 @@
-
 package org.perfidix.visitor;
 
 import java.io.File;
@@ -18,76 +17,73 @@ import org.perfidix.Result;
  */
 public class RawData extends ResultVisitor {
 
-  private final File folder;
+    private final File       folder;
 
-  private static final Log LOGGER = LogFactory.getLog(RawData.class);
+    private static final Log LOGGER = LogFactory.getLog(RawData.class);
 
-  public RawData(final String pathToFolder) {
+    public RawData(final String pathToFolder) {
 
-    folder = new File(pathToFolder);
-    if (folder.exists()) {
-      if (!folder.isDirectory()) {
-        folder.delete();
-      } else {
-        deleteRecursive(folder);
-      }
-    }
-    folder.mkdir();
-  }
-
-  @Override
-  public void visit(Result r) {
-
-    if (!(r instanceof IResult.BenchmarkResult)) {
-      throw new RuntimeException("only benchmark results are supported!");
-    }
-    IResult.BenchmarkResult benchRes = (IResult.BenchmarkResult) r;
-    for (IResult.ClassResult classRes : benchRes.getChildren()) {
-      for (IResult.MethodResult methodRes : classRes.getChildren()) {
-        getMethodResult(methodRes);
-      }
-    }
-  }
-
-  private void getMethodResult(final IResult.MethodResult methodRes) {
-
-    try {
-      File currentFile = new File(this.folder.getAbsolutePath()
-          + File.separatorChar + methodRes.getName());
-      while (currentFile.exists()) {
-        LOGGER.warn(currentFile.getAbsolutePath() + " is already existing!");
-        currentFile = new File(this.folder.getAbsolutePath()
-            + File.separatorChar + methodRes.getName());
-      }
-      LOGGER.info("Using " + currentFile.getAbsolutePath() + " for output!");
-      currentFile.createNewFile();
-      final FileWriter out = new FileWriter(currentFile);
-      IResult.SingleResult single = methodRes.getChildren().get(0);
-      final long data[] = single.getResultSet();
-      for (int i = 0; i < data.length; i++) {
-        if (i == data.length - 1) {
-          out.write(data[i] + " ");
-        } else {
-          out.write(data[i] + ",");
+        folder = new File(pathToFolder);
+        if (folder.exists()) {
+            if (!folder.isDirectory()) {
+                folder.delete();
+            } else {
+                deleteRecursive(folder);
+            }
         }
-      }
-      out.flush();
-      out.close();
-    } catch (Exception e) {
-      e.printStackTrace();
+        folder.mkdir();
     }
-  }
 
-  private static void deleteRecursive(final File file) {
-
-    if (file.isDirectory()) {
-      final File[] childs = file.listFiles();
-      for (int i = 0; i < childs.length; i++) {
-        deleteRecursive(childs[i]);
-      }
-    } else {
-      file.delete();
+    @Override
+    public void visit(Result r) {
+        try {
+            if (!(r instanceof IResult.BenchmarkResult)) {
+                throw new RuntimeException("only benchmark results are supported!");
+            }
+            IResult.BenchmarkResult benchRes = (IResult.BenchmarkResult) r;
+            for (IResult.ClassResult classRes : benchRes.getChildren()) {
+                for (IResult.MethodResult methodRes : classRes.getChildren()) {
+                    File timeFile = new File(this.folder.getAbsolutePath() + File.separatorChar + classRes.getName()
+                            + "$" + methodRes.getName());
+                    timeFile.createNewFile();
+                    getMethodResult(timeFile, methodRes);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-  }
+
+    private void getMethodResult(final File outputFile, final IResult.MethodResult methodRes) {
+
+        try {
+            final FileWriter timeOut = new FileWriter(outputFile);
+            IResult.SingleResult singleTime = methodRes.getChildren().get(0);
+            final long data[] = singleTime.getResultSet();
+            for (int i = 0; i < data.length; i++) {
+                if (i == data.length - 1) {
+                    timeOut.write(data[i] + " ");
+                } else {
+                    timeOut.write(data[i] + ",");
+                }
+            }
+            timeOut.flush();
+            timeOut.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void deleteRecursive(final File file) {
+
+        if (file.isDirectory()) {
+            final File[] childs = file.listFiles();
+            for (int i = 0; i < childs.length; i++) {
+                deleteRecursive(childs[i]);
+            }
+        } else {
+            file.delete();
+        }
+    }
 
 }
