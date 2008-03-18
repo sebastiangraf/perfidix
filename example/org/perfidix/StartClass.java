aedd549be7fad887b9933522f431e4192f1df7f9
sebastiangraf <devnull@localhost>
2008-03-18 15:26:26 +0000

    fixed visibility of getInstance of Perfidix

--- example/org/perfidix/StartClass.java
+++ example/org/perfidix/StartClass.java
@@ -5,8 +5,18 @@ import org.perfidix.visitor.AsciiTable;
 public class StartClass {
 
     public static void main(String[] args) {
-
-        final Benchmark bench = new Benchmark(true);
+        final IMeter countingmeter = Perfidix.createMeter("test", "t");
+        countingmeter.tick();
+        countingmeter.tick();
+        countingmeter.tick();
+        countingmeter.tick();
+        countingmeter.tick();
+        countingmeter.tick();
+        countingmeter.tick();
+        countingmeter.tick();
+        countingmeter.tick();
+        final Benchmark bench = new Benchmark();
+        bench.register(countingmeter);
         bench.add(new ClassAnnoBenchmark());
         bench.add(new SomeAnnoBenchmark());
         bench.add(new SomeSpecificSetUpTearDownBenchmark());
