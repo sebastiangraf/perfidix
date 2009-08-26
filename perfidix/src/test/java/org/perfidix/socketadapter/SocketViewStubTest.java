d66e35778cb4a461ca655d513e10b7de8fc7d83f
lewandowskil <none@none>
2009-08-26 09:15:37 +0000

    renamed test folder ideadapter to socketadapter.
    --HG--
    extra : convert_revision : svn%3Af0255af0-1427-0410-b2be-e7b72c6cd545/trunk%40339

--- perfidix/src/test/java/org/perfidix/socketadapter/SocketViewStubTest.java
+++ perfidix/src/test/java/org/perfidix/socketadapter/SocketViewStubTest.java
@@ -18,7 +18,7 @@
  * $LastChangedDate$
  *
  */
-package org.perfidix.ideadapter;
+package org.perfidix.socketadapter;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
